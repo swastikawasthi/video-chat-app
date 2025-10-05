ced5895aedc93fb380e84db05e9bf6631b577e0f
swastikawasthi <142707726+swastikawasthi@users.noreply.github.com>
2025-10-05 03:26:02 +0000

    Update message

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static('./'));
+
+io.on('connection', socket => {
+    socket.on('join', room => {
+        socket.join(room);
+        socket.to(room).emit('user-connected', socket.id);
+
+        socket.on('offer', data => {
+            socket.to(room).emit('offer', data);
+        });
+
+        socket.on('answer', data => {
+            socket.to(room).emit('answer', data);
+        });
+
+        socket.on('ice-candidate', data => {
+            socket.to(room).emit('ice-candidate', data);
+        });
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
